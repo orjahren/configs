630ee6241012d7fe5bd29d2efdead0ffbbbcfaa8
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-05-18 16:23:39 +0200

    Use more LSPs

--- nvim/init.lua
+++ nvim/init.lua
@@ -393,11 +393,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
+  clangd = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
+  tsserver = {},
 
   lua_ls = {
     Lua = {
