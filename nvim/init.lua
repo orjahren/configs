b0f5368293b79b82f643d38fbc2aed6f1b9dc710
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-05-18 15:51:31 +0200

    Use file tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -114,7 +114,16 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
